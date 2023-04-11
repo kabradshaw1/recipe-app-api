91ad3541a539df43b2d12f08de20a7c08898645d
kabradshaw1 <itllbok82@gmail.com>
2023-04-11 11:01:51 -0400

    writing tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,7 @@ Calculator functions
 def add(x, y):
   """Add x and y and return results."""
   return x + y
+
+def subract(x, y):
+  """Subract x from y and return reslt"""
+  return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
     res = calc.add(5, 6)
 
     self.assertEqual(res, 11)
+
+  def test_subtract_number(self):
+    """test subract numbers"""
+    res = calc.subract(10, 15)
+
+    self.assertEqual(res, 5)
