95f4cc7c81da6a39b5574c907cacfd747b9a8696
kabradshaw1 <itllbok82@gmail.com>
2023-04-12 09:52:58 -0400

    working on tutorial

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 Calculator functions
 """
 
-def add(x, y):
-  """Add x and y and return results."""
-  return x + y
 
-def subract(x, y):
-  """Subract x from y and return reslt"""
-  return y - x
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
