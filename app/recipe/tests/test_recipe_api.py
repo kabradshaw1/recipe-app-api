21b3e7fa293502e585969b25355d6c717bd24ba9
kabradshaw1 <itllbok82@gmail.com>
2023-05-01 18:22:54 -0400

    recipe tests done

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,42 @@
+"""
+Tests for recipe APIs.
+"""
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+
+from recipe.serializers import RecipeSerializer
+
+RECIPE_URL = reverse('recipe:recipe-list')
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe."""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 22,
+        'price': Decimal('5.25'),
+        'description': 'Sample discription',
+        'link': 'http://example.com/recipe.pdf'
+    }
+    defaults.update(params)
+
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
+
+class PublicRecipeAPITests(TestCase):
+    """Test unauthenticated API requests."""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test Auth required to call API"""
+        res = self.client.get(RECIPE_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
