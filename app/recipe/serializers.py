c2ba69ee0b4409fd13c1d91047b94804ed6843ff
kabradshaw1 <itllbok82@gmail.com>
2023-05-02 09:08:44 -0400

    working on recipe view and serializer

---
+++ app/recipe/serializers.py
@@ -0,0 +1,13 @@
+"""
+Serializres for recipe APIs
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe."""
+
+    class Meta:
+        model = Recipe
+        field = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -40,3 +40,42 @@ class PublicRecipeAPITests(TestCase):
         res = self.client.get(RECIPE_URL)
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateRecipeAPITests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'user@example.com',
+            'testpass123'
+        )
+        self.client.force_authenticate(self.user)
+
+    def test_retrive_recipes(self):
+        """Test retrieving a list of recipes."""
+        create_recipe(user=self.user)
+        create_recipe(user=self.user)
+
+        res = self.client.get(RECIPE_URL)
+
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_recipe_list_limited_to_user(self):
+        """Test list of recipes is limited to authenticated user."""
+        other_user = get_user_model().objects.create_user(
+            'other@example.com',
+            'password123'
+        )
+        create_recipe(user=other_user)
+        create_recipe(user=self.user)
+
+        res = self.client.get(RECIPE_URL)
+
+        recipes = Recipe.objects.filts(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
